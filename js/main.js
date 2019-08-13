7c81022979fd21e8a420f0d377c0ee03d7af00e7
Faruk <15935486+faluoke@users.noreply.github.com>
2019-08-12 20:58:10 -0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,45 @@
+$(document).ready(() => {
+  const $charCount = $("#tweet-modal .char-count");
+  const maxCharCount = parseInt($charCount.data("max"), 10);
+
+  $("#tweet-modal textarea").on("keyup", e => {
+    const tweetLength = $(e.currentTarget).val().length;
+
+    $charCount.html(maxCharCount - tweetLength);
+  });
+  $('[data-toggle="tooltip"]').tooltip();
+  const popoverContentTemplate =
+    "" +
+    '<img src="imgs/breed.jpg" class="img-rounded">' +
+    '<div class="info">' +
+    "<strong>Dog Breeds</strong>" +
+    '<a href="#" class="btn btn-default">' +
+    '<span class="glyphicon glyphicon-plus" aria-hidden="true"></span>' +
+    "Follow" +
+    "</a>" +
+    "</div>";
+
+  $('[data-toggle="popover"]').popover({
+    placement: "bottom",
+    html: true,
+    content: () => {
+      return popoverContentTemplate;
+    }
+  });
+  $('[data-toggle="popover"]').on("show.bs.popover", function() {
+    var $icon = $(this).find("span.glyphicon");
+
+    $icon.removeClass("glyphicon-plus").addClass("glyphicon-ok");
+    $(this).append("ing");
+  });
+
+  $("#profile")
+    .on("affix.bs.affix", function() {
+      $(this).width($(this).width() - 1);
+      $("#main").addClass("col-md-offset-3");
+    })
+    .on("affix-top.bs.affix", function() {
+      $(this).css("width", "");
+      $("#main").removeClass("col-md-offset-3");
+    });
+});
